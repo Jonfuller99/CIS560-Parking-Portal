1b6566a39a3e1c26e07a296a7a21e0215930df96
ckbartel <ckbartel@ksu.edu>
2025-04-26 21:43:14 -0500

    added a bunch more tickets!!!

---
+++ SQL/StoredProcedures/Aggregating Queries/OfficerRank.sql
@@ -0,0 +1,27 @@
+DROP PROCEDURE IF EXISTS Parking.OfficerRank;
+
+GO
+
+CREATE PROCEDURE Parking.OfficerRank
+    @Month INT,
+    @Year INT
+    
+AS
+BEGIN
+    SELECT 
+        RANK() OVER(ORDER BY SUM(ISNULL(T.Fee, 0) + ISNULL(T.LateCharge, 0)) DESC, COUNT(*) DESC) AS OfficerRank,
+        O.LastName,
+        O.FirstName,
+        COUNT(T.TicketID) AS TicketCount,
+        SUM(ISNULL(T.Fee, 0) + ISNULL(T.LateCharge, 0)) AS TicketRevenue
+    FROM Parking.Tickets T
+        RIGHT JOIN Parking.Officers O ON O.OfficerID = T.OfficerID
+            AND MONTH(T.TimeIssued) = @Month
+            AND YEAR(T.TimeIssued) = @Year
+    GROUP BY T.OfficerID, O.LastName, O.FirstName
+    ORDER BY OfficerRank
+END;
+
+GO
+
+EXEC Parking.OfficerRank @Month = 5, @Year = 2024;
